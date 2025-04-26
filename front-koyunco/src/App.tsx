e5fc75e78e0cb7062c461bed6783f53b438b6229
Koyunco <info@koyunco.com>
2025-04-26 08:38:23 +0200

    App single page

--- front-koyunco/src/App.tsx
+++ front-koyunco/src/App.tsx
@@ -1,4 +1,4 @@
-
+import Header from './components/Header'
 import Home from './Home'
 import Product from './Product'
 import Footer from "./components/Footer"
@@ -7,7 +7,7 @@ import Footer from "./components/Footer"
 function App() {
   return (
     <>
-
+      <Header></Header>
       <Product></Product>
       <Footer></Footer>
     </>
